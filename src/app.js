34e03b8bfa926d8245414893d159423802ae0916
saimonMVN <sultan@moveon.com.bd>
2025-04-22 22:58:57 +0600

    check

--- src/app.js
+++ src/app.js
@@ -15,7 +15,27 @@ const app = express();
 // Middleware
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(cors());
+const allowedOrigins = [
+    'https://adminsadnan.netlify.app',
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost:5001'
+];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', "PUT"],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+};
+
+app.use(cors(corsOptions));
 
 // Routes - import routes directly here to ensure they're loaded correctly
 const userRoutes = require('./routes/userRoutes');
